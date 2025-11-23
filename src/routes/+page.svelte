8e3280baaf1c35f50fdc12fe05ef9f42cd3a1fa6
SEOL_DESKTOP\seoll <seollove@gmail.com>
2025-11-23 14:22:37 +0900

    동영상 변경

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,7 +42,7 @@
 <section class="hero section visible" id="hero">
 	<div class="hero-overlay"></div>
 	<video class="hero-video" autoplay muted loop>
-		<source src="/background_01.mp4" type="video/mp4">
+		<source src="/background_02.mp4" type="video/mp4">
 		Your browser does not support the video tag.
 	</video>
 	<div class="hero-content">
